19178f957103ef517ecc6108d4350a9ee57899b8
zhongwen <luotianyi1997@gmail.com>
2019-12-06 11:52:52 +0800

    Update #952.cpp

--- #952.cpp
+++ #952.cpp
@@ -1,39 +1,58 @@
+//没考虑无素数情况
 class Solution {
 public:
     int flag=0;
-    vector<vector<int>> Prime(0,vector<int>(1))
+    vector<vector<int>> Prime;
+    vector<int> v;
     bool isPrimeNumber(int a){
         if(a<=2){
             return true;
         }
         for (int i=2;i<a;i++){
-            if(a%i==0) return true;
+            if(a%i==0) return false;
+        }
+        return true;
+    }
+    //Find
+    bool isSameExist(vector<int> A,vector<int> B){
+        if(A.size()==1||B.size()==1) return false;
+        for (auto it=B.begin();it<B.end();it++){
+            if(find(A.begin(),A.end(),*it)!=A.end()){
+                return true;
+            }
         }
         return false;
     }
-    bool isSameExist(vector<int> A,vector<int> B){}
-    void mergeAB(vector<int> A,vector<int> B){}
+    //Merge
+    void mergeAB(vector<int>& A,vector<int>& B){
+        A.insert(A.end(),B.begin(),B.end());
+        sort(A.begin(),A.end());
+        auto iter = unique(A.begin(), A.end());
+        A.erase(iter,A.end());
+        B.erase(B.begin()+1,B.end());
+        return ;
+    }
     int largestComponentSize(vector<int>& A) {
         if (A.size()<=1) return A.size();
         for(auto it=A.begin();it<A.end();it++){
+            v.clear();
             if(isPrimeNumber(*it)){
                 flag++;
-                Prime.resize(flag);
-                Prime[flag].push_back(*it);
+                v.push_back(*it);
+                Prime.push_back(v);
                 *it=-1;
             }
         }
         for(int i=0;i<flag;i++){
             for(auto it=A.begin();it<A.end();it++){
                 if(*it%Prime[i][0]==0){
-                    Prime[i].push_back(*it)
-                    *it=-1;
+                    Prime[i].push_back(*it);
                 }
             }
         }
         for(int i=0;i<flag-1;i++){
             for(int j=i+1;j<flag;j++){
-                if(isSameExist(Prime[i],Prime[j]) mergeAB(Prime[i],Prime[j]);
+                if(isSameExist(Prime[i],Prime[j])) mergeAB(Prime[i],Prime[j]);
             }
         }
         int max;
